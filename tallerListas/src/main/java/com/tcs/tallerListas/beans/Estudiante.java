20b3ce2d37ae41be28984e870bed947d4c2188f6
David <73247807+dapalaci@users.noreply.github.com>
2021-08-17 17:22:30 -0500

    Add files via upload

---
+++ tallerListas/src/main/java/com/tcs/tallerListas/beans/Estudiante.java
@@ -0,0 +1,42 @@
+package com.tcs.tallerListas.beans;
+
+public class Estudiante {
+	 
+	private String nroId;
+	private String nombre;
+	private Integer edad;
+	private String estado;
+	private String grado;
+	
+	public String getNroId() {
+		return nroId;
+	}
+	public void setNroId(String nroId) {
+		this.nroId = nroId;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Integer getEdad() {
+		return edad;
+	}
+	public void setEdad(Integer edad) {
+		this.edad = edad;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	public String getGrado() {
+		return grado;
+	}
+	public void setGrado(String grado) {
+		this.grado = grado;
+	}
+
+}
